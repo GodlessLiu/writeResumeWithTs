8cf08edf1b5be45b0d401c3717cd590f7886868b
GodlessLiu <110895612+GodlessLiu@users.noreply.github.com>
2022-11-16 01:17:49 +0800

    modify

--- src/example/temp1.ts
+++ src/example/temp1.ts
@@ -1,12 +1,12 @@
 export const t = ():temp1Type => {
     return {
-        "name": "刘爱丰",
+        "name": "刘ee",
         "identity": "24届应届生",
         "headerLogo":"https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fqcloud.dpfile.com%2Fpc%2F_aIv6BRPbrByvG4oCPBReXNu5pkaS0cFF6mygszhgD2OS9KTf3d-j_wdvfcJFGnLTYGVDmosZWTLal1WbWRW3A.jpg&refer=http%3A%2F%2Fqcloud.dpfile.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1671114891&t=4120375eb1ff68e47479f1bd9af56a5a",
         "contactMe": {
-            "phone": 15390335923,
-            "email": "2788370451@qq.com",
-            "where": "四川成都"
+            "phone": 153903352222,
+            "email": "2788372221@qq.com",
+            "where": "四川"
         },
         "age": 21,
         "JobHuntingIntention": "前端工程师",
@@ -14,7 +14,7 @@ export const t = ():temp1Type => {
             {
                 "title": "教育经历",
                 "header": {
-                    "title": "四川农业大学（211，双一流） 计算机技术与科学",
+                    "title": "四川计算机技术与科学",
                     "time": {
                         "start": "2022年6月30日",
                         "end": "2022年7月30日"
@@ -29,12 +29,12 @@ export const t = ():temp1Type => {
             {
                 "title": "项目经历",
                 "header": {
-                    "title": "sap赋能中心管理平台"
+                    "title": "赋能中心管理平台"
                 },
                 "content": [
                     {
                         "name": "项目介绍：",
-                        "describe": "该项目主要包含门户网站和项目接单平台两部分，项目接单平台主要功能是完成不同企业在该平 台上发布的需求。该项目主要包含门户网站和项目接单平台两部分，项目接单平台主要功能是完成不同企业在该平 台上发布的需求。该项目主要包含门户网站和项目接单平台两部分，项目接单平台主要功能是完成不同企业在该平 台上发布的需求。"
+                        "describe": "不同企业在该平 台上发布的需求。该项目主要包含门户网站和项目接单平台两部分，项目接单平台主要功能是完成不同企业在该平 台上发布的需求。该项目主要包含门户网站和项目接单平台两部分，项目接单平台主要功能是完成不同企业在该平 台上发布的需求。"
                     },
                     {
                         "name": "所用技术：",
@@ -50,19 +50,19 @@ export const t = ():temp1Type => {
                 "title": "专业技能",
                 "content":[
                     {
-                        describe:'熟练使用 Javascript 进行编程，对异步编程有较为深入的理解，熟练掌握 ES5/ES6+语法'
+                        describe:'熟练的理解，熟练掌握 ES5/ES6+语法'
                     },
                     {
-                        describe:'熟练使用 Vue2/Vue3 进行项目开发'
+                        describe:'熟 Vue2/Vue3 进行项目开发'
                     },
                     {
-                        describe:'熟练使用 Webpack 对项目进行打包'
+                        describe:'Webpack 对项目进行打包'
                     },
                     {
-                        describe:'理解 HTTP 协议以及数据结构算法相关知识'
+                        describe:'理解 以及数据结构算法相关知识'
                     },
                     {
-                        describe:'能够使用 Git 进行协同开发，以及了解 git flow 工作流'
+                        describe:'能够使用 G了解 git flow 工作流'
                     }
                 ]
 
